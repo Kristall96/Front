fe239a7245b7ad4c59bd20634eb52571ac3e7572
Kristall96 <erkandev@outlook.com>
2025-05-24 13:54:46 +0300

    asd

--- src/utils/secureAxios.js
+++ src/utils/secureAxios.js
@@ -2,8 +2,9 @@
 import axios from "axios";
 
 const secureAxios = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true, // send/receive cookies (for refreshToken)
 });
+console.log("✔️ Axios baseURL:", import.meta.env.VITE_API_URL);
 
 export default secureAxios;
