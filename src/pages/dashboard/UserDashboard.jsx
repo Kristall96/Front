0799260be814063fd15d26ee4e3e035913613117
Kristall96 <erkandev@outlook.com>
2025-05-25 16:36:13 +0300

    asd

--- src/pages/dashboard/UserDashboard.jsx
+++ src/pages/dashboard/UserDashboard.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import secureAxios from "../../utils/secureAxios";
 import DashboardLayout from "../dashboard/DashboardLayout";
-
+import Navbar from "../../components/Navbar";
 const UserDashboard = () => {
   const [data, setData] = useState(null);
 
@@ -10,19 +10,22 @@ const UserDashboard = () => {
   }, []);
 
   return (
-    <DashboardLayout>
-      <h1 className="text-2xl font-bold mb-4">User Dashboard</h1>
-      {data ? (
-        <div className="bg-white p-4 rounded shadow">
-          <p>{data.message}</p>
-          <pre className="mt-4 text-sm bg-gray-100 p-4 rounded">
-            {JSON.stringify(data.recentOrders, null, 2)}
-          </pre>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </DashboardLayout>
+    <>
+      <Navbar />
+      <DashboardLayout>
+        <h1 className="text-2xl font-bold mb-4">User Dashboard</h1>
+        {data ? (
+          <div className="bg-white p-4 rounded shadow">
+            <p>{data.message}</p>
+            <pre className="mt-4 text-sm bg-gray-100 p-4 rounded">
+              {JSON.stringify(data.recentOrders, null, 2)}
+            </pre>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </DashboardLayout>
+    </>
   );
 };
 
