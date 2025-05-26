3b327ac2266b36047bcca81e67608519fec9c3f1
Kristall96 <erkandev@outlook.com>
2025-05-26 12:15:41 +0300

    asd

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import axios from "../../utils/secureAxios";
+import axios from "../utils/secureAxios";
 
 const SingleProductPage = () => {
   const { slug } = useParams();
