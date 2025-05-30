b5f4a89895e1384624622c2d92b4aaea80d60c87
Kristall96 <erkandev@outlook.com>
2025-05-30 18:42:06 +0300

    asd

--- src/pages/dashboard/sections/ProductManagement/ProductForm.jsx
+++ src/pages/dashboard/sections/ProductManagement/ProductForm.jsx
@@ -199,9 +199,7 @@ const ProductForm = ({ onSubmit, initialData = {} }) => {
             <Label>{label}</Label>
             <input
               type="number"
-              inputMode="decimal"
-              className="appearance-none [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none 
-             w-full px-3 py-2 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="no-spinner w-full px-3 py-2 rounded-md bg-gray-800 text-white border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
               value={form[key] || ""}
               onChange={(e) =>
                 setForm({ ...form, [key]: +e.target.value || 0 })
