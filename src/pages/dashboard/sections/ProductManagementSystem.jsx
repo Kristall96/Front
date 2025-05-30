fe9e715d3e8286165d0ce36c7994137d483ef253
Kristall96 <erkandev@outlook.com>
2025-05-30 10:15:13 +0300

    asd

--- src/pages/dashboard/sections/ProductManagementSystem.jsx
+++ src/pages/dashboard/sections/ProductManagementSystem.jsx
@@ -16,7 +16,7 @@ const ProductManagementSystem = () => {
 
   return (
     <section>
-      <h1 className="text-2xl font-bold text-gray-900 dark:text-white mb-6">
+      <h1 className="text-2xl font-bold text-gray-400 dark:text-white mb-6">
         All Products
       </h1>
 
